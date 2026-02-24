182e46de1f54bdd5f255c8c6606beaa31ee9feeb
Nihal <p.b.nihalcse1@gmail.com>
2026-02-24 22:08:13 +0600

    Initial commit

---
+++ api/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path, include
+from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet, ProductImageViewSet
+from order.views import CartViewSet, CartItemViewSet, OrderViewset, initiate_payment,payment_cancel, payment_fail, payment_success, HasOrderedProduct
+from rest_framework_nested import routers
+
+router = routers.DefaultRouter()
+router.register('products', ProductViewSet, basename='products')
+router.register('categories', CategoryViewSet)
+router.register('carts', CartViewSet, basename='carts')
+router.register('orders', OrderViewset, basename='orders')
+
+product_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+product_router.register('reviews', ReviewViewSet, basename='product-review')
+product_router.register('images', ProductImageViewSet,
+                        basename='product-images')
+
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register('items', CartItemViewSet, basename='cart-item')
+
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(product_router.urls)),
+    path('', include(cart_router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path("payment/initiate/", initiate_payment, name="initiate-payment"),
+    path("payment/success/", payment_success, name="payment-success"),
+    path("payment/fail/", payment_fail, name="payment-fail"),
+    path("payment/cancel/", payment_cancel, name="payment-cancel"),
+    path('orders/has-ordered/<int:product_id>/',
+         HasOrderedProduct.as_view()),
+]
